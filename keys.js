4dc2600ca450b12aea486bebdc754cfe4a2355db
rknLA <kevin@rkn.la>
2012-10-21 18:29:00 -0700

    Add template keys file.

---
+++ keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  api_keys: {
+    spotify: '[your spotify key here]',
+    rdio: '[your rdio key here]',
+    bandcamp: '[your bandcamp key here]',
+    soundcloud: '[your soundcloud key here]'
+  }
+};
